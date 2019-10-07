38ceb2e50c1946dd4838e52fe9dfbf35abc7e4a4
Tochukwu Nwachukwu <truetochukz@gmail.com>
2019-10-07 06:56:29 +0200

    Recipe App - React with JSX

---
+++ learning-react/chp5/recipe-app/src/components/recipe.js
@@ -0,0 +1,19 @@
+import IngredentList from './ingredientList';
+import Instructions from './instructions';
+
+const sectionStyle = {
+  border: 'solid white 1px',
+  padding: '1em',
+  margin: '1em',
+  width: '50%'
+}
+const Recipe = ({name, ingredients, steps}) =>
+    <section id={name.toLowerCase().replace(/ /g, '-')} style={sectionStyle}>
+        <h2>{name}</h2>
+        <h3>Ingredients:</h3>
+        <IngredentList  list={ingredients} />
+        <Instructions title="Cooking Instructions" steps={steps} />
+    </section>
+
+
+export default Recipe;
