81e3930e4b93e07f75a450ea9596c559e64e3566
Tochukwu Nwachukwu <truetochukz@gmail.com>
2019-10-07 06:58:37 +0200

    Recipe app - React JSX

--- learning-react/recipe-demo/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Menu from './components/menu';
-import data from './data/recipes';
-//import Summary from './components/summary';
-
-window.React = React;
-ReactDOM.render(<Menu recipes={data} />, document.getElementById('react-container'));
-
